8b54acd7796c659da3e13ac2414a7e37a30942a5
cyhaz <cyhaz@naver.com>
2020-03-17 15:46:23 +0900

    비밀번호에 따른 로그인성공유무 토스트로 띄우기

--- app/src/main/java/kr/co/tjoeun/a20200317_java01/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200317_java01/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import kr.co.tjoeun.a20200317_java01.databinding.ActivityMainBinding;
 
@@ -32,9 +33,32 @@ public class MainActivity extends AppCompatActivity {
 
                 String input = binding.inputEdt.getText().toString();
 
+                Log.d("입력값확인", input);
+
                 binding.messageTxt.setText(input);
                 binding.inputEdt.setText("");
 
+            }
+        });
+
+
+//        입력한 비밀번호가 1234 일때 로그인버튼을 누르면
+//        토스트로 로그인 성공 출력
+//        그 외의 비밀번호로 로그인버튼 누르면
+//        토스트로 잘못된 비밀번호입니다 출력
+
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 입력한 값을 변수로 저장
+                String pw = binding.pwEdt.getText().toString();
+                // 비밀번호가 1234인가?
+                // String을 ==으로 처리하면 에러! equals 활용
+                if (pw.equals("1234")) {
+                    Toast.makeText(MainActivity.this, "로그인 성공!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "잘못된 비밀번호입니다.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
